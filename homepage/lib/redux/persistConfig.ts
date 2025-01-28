eba3ab9f0a6e9988967a1dfdbc30e85ae16d6fc7
Dinesh Yadav <dineshnirban01@gmail.com>
2025-01-28 17:57:02 +0530

    edit persist storage

--- homepage/lib/redux/persistConfig.ts
+++ homepage/lib/redux/persistConfig.ts
@@ -1,23 +1,10 @@
-import createWebStorage from 'redux-persist/lib/storage/createWebStorage';
-import { WebStorage } from 'redux-persist/lib/types';
+declare module 'redux-persist/lib/storage/createWebStorage' {
+    type WebStorage = {
+        getItem: (key: string) => Promise<string | null>;
+        setItem: (key: string, value: string) => Promise<void>;
+        removeItem: (key: string) => Promise<void>;
+    };
 
-export function createPersistStorage(): WebStorage {
-    const isServer = typeof window === 'undefined';
-
-    // Returns noop (dummy) storage.
-    if (isServer) {
-        return {
-            getItem(_key: string): Promise<string | null> {
-                return Promise.resolve(null); // This is fine, as it returns null in the absence of a value.
-            },
-            setItem(_key: string, value: string): Promise<void> {
-                return Promise.resolve(); // No-op
-            },
-            removeItem(_key: string): Promise<void> {
-                return Promise.resolve(); // No-op
-            },
-        };
-    }
-
-    return createWebStorage('local'); // Normal behavior for browser environment
+    const createWebStorage: (type: 'local' | 'session') => WebStorage;
+    export default createWebStorage;
 }
---
+++ homepage/lib/types/redux-persists.d.ts
@@ -0,0 +1,4 @@
+// declare module 'redux-persist/lib/storage/createWebStorage' {
+//     const createWebStorage: (type: string) => any;
+//     export default createWebStorage;
+// }
